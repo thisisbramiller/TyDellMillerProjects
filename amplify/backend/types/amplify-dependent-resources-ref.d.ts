c9a8b6b95d6632a8f91c8a86388c392bbdd5f2e7
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-16 13:04:51 -0500

    feat: :bricks: add storage configurations

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3tydellmillerprojectsstorage2dddd843": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
