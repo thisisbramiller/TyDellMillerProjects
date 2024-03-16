af5c9eb690107602f8605b6165a8c0f44974518d
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-16 17:51:40 -0500

    fix: :ambulance: fix dev env rebuild infra

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "tydellmillerprojects": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3tydellmillerprojectsstorage2dddd843": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
