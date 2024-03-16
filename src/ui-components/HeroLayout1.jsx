00a3c70225e69ae782d9b3d281576ec287211ba7
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-16 14:58:43 -0500

    feat: :bricks: add dev, test, and prod environments in AWS Amplify

--- src/ui-components/HeroLayout1.jsx
+++ src/ui-components/HeroLayout1.jsx
@@ -16,7 +16,6 @@ export default function HeroLayout1(props) {
   const { overrides: overridesProp, ...rest } = props;
   const variants = [
     {
-      variantValues: { mode: "Light" },
       overrides: {
         Eyebrow: {},
         Heading: {},
@@ -29,9 +28,9 @@ export default function HeroLayout1(props) {
         Right: {},
         HeroLayout1: {},
       },
+      variantValues: { mode: "Light" },
     },
     {
-      variantValues: { mode: "Dark" },
       overrides: {
         Eyebrow: { color: "rgba(188,236,245,1)", children: "Full-stack" },
         Heading: { color: "rgba(255,255,255,1)" },
@@ -44,6 +43,7 @@ export default function HeroLayout1(props) {
         Right: {},
         HeroLayout1: {},
       },
+      variantValues: { mode: "Dark" },
     },
   ];
   const overrides = mergeVariantsAndOverrides(
