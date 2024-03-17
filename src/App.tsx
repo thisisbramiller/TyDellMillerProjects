71ff7d7fc238b804aa1f344911f2b6867bc36625
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-16 19:23:27 -0500

    feat: :truck: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import NavBarHeader from './ui-components/NavBarHeader'
 import '@aws-amplify/ui-react/styles.css'
+import HomePage from './pages/HomePage'
 
 
 function App() {
   return (
-    <NavBarHeader />
+    <BrowserRouter>
+        <NavBarHeader width="100%" />
+      <Routes>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
