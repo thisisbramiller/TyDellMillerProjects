444095a71d75aa20f60f55a05c3be87b2e66c19f
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-03-16 22:49:31 -0500

    feat: :lipstick: Add About and Features2x2 components to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,14 @@
+import Features2x2 from "../ui-components/Features2x2";
 import HeroLayout1 from "../ui-components/HeroLayout1";
+import About from "../ui-components/About";
 
 function HomePage() {
     return (
-      <HeroLayout1 width={"100%"} marginBottom="20px"/>  
+      <>
+        <HeroLayout1 width={"100%"} marginBottom="20px"/>
+        <About />
+        <Features2x2 />
+      </>
     );
 }
 
---
+++ src/ui-components/About.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Flex, Grid, Text, Heading } from '@aws-amplify/ui-react'
+
+function About() {
+return (
+    <section className="about">
+        <Flex alignItems="center" justifyContent="center">
+            <Grid columnGap="0.5rem" rowGap="0.5rem" templateColumns="1fr" templateRows="1fr">
+                <Heading textAlign="center" level={1}>About Us</Heading>
+                <Text>Provide a brief overview of your business, its mission, values, and what sets it apart from competitors.</Text>
+            </Grid>
+        </Flex>
+    </section>
+);
+}
+
+export default About;
