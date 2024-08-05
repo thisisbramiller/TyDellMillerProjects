0013c1f245df55104c6df0cea896b63eae72451a
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2024-08-05 14:13:15 -0500

    feat: update amplify and change landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import logo from './assets/logo.png'
-import viteLogo from '../public/vite.svg'
-import reactLogo from './assets/react.svg'
-import typescriptLogo from './assets/typescript.svg'
 import './App.css'
 
 function App() {
@@ -9,22 +6,11 @@ function App() {
     <>
       <div>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="react logo" />
+          <img src={logo} className="App-logo" alt="tydell miller logo" />
         </header>
       </div>
       <br/>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://www.typescriptlang.org/" target="_blank">
-          <img src={typescriptLogo} className="logo typescript" alt="Typescript logo" />
-        </a>
-      </div>
-      <h1>Built with Vite + React + Typescript</h1>
+      <h1>Software | DevSecOps | Technologist | Musician</h1>
     </>
   )
 }
